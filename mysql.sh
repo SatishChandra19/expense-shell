be93139601be017cc2d3a5025d816ad6777ea207
Satishchandra19 <satishchandra1997@gmail.com>
2025-03-20 01:28:50 +0000

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -42,5 +42,12 @@ VALIDATE $? "enabling MySql server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting MySQL server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $! "setting Root Password"
+
+if[ $? -ne 0 ]
+then
+    echo "MySQL Root password not setup" &>>$LOG_FILE_NAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $! "setting Root Password"
+else
+    echo "MySQL Root password already setup ...  SKIPPING"
+fi
