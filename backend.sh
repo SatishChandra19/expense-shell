ced4ea600459c7128de896299116d3f5ca57da12
Satishchandra19 <satishchandra1997@gmail.com>
2025-03-24 00:07:17 +0000

    expense shell

--- backend.sh
+++ backend.sh
@@ -73,7 +73,7 @@ cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.serv
 dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MySQL client"
 
-mysql -h mysql.devopz.online -uroot -pExpenseApp@1 /app/schema/backend.sql &>>$LOG_FILE_NAME
+mysql -h mysql.devopz.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 VALIDATE $?  "setting up the transactions schema and tables"
 
 systemctl daemon-reload &>>$LOG_FILE_NAME
